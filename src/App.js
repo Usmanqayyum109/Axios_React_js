9e65d991f8a218b7dda9c6d1317293ced18d8881
Usmanqayyum109 <you@example.com>
2023-03-05 16:36:02 +0500

    axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const [num, setNum] = useState();
+  const [name, setName] = useState();
+  const [id, setId] = useState();
+  const [height, setHeight] = useState();
+
+  useEffect(() => {
+    // alert("hi");
+    async function getData() {
+      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/ditto`);
+      console.log(res.data.name);
+      setName(res.data.name);
+      setId(res.data.id);
+      setHeight(res.data.height);
+    }
+    getData();
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        {name} {id} {height}
+      </h1>
+      <select
+        value={num}
+        onChange={(event) => {
+          setNum(event.target.value);
+        }}
+      >
+        <option value="1">1</option>
+        <option value="25">25</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
     </div>
   );
 }
-
-export default App;
